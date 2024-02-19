a797926698585b070b6df64b7327fa02ab2c4a4b
am07127 <am07127@st.habib.edu.pk>
2024-02-19 10:09:14 +0500

    small change

--- client/app/components/Nav.tsx
+++ client/app/components/Nav.tsx
@@ -51,7 +51,7 @@ export const Nav = () => {
                 stroke="currentColor"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                stroke-width="2"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
