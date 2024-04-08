a310a89ed4da9c88c2a1ab07268026d0fbfebd9a
am07127 <am07127@st.habib.edu.pk>
2024-04-08 09:58:03 +0500

    merge resolve

--- client/lib/redux/rootReducer.ts
+++ client/lib/redux/rootReducer.ts
@@ -1,11 +1,13 @@
 /* Instruments */
-import { counterSlice, orderSlice, cartSlice,itemSlice } from "./slices";
+import { counterSlice, orderSlice, cartSlice,itemSlice, authSlice } from "./slices";
 // import { cartSlice } from "./slices";
 // import { itemSlice } from "./slices";
 // import { orderSlice } from "./slices";
-import { authSlice } from "./slices";
 
 export const reducer = {
   counter: counterSlice.reducer,
   cart: cartSlice.reducer,
+  items: itemSlice.reducer,
+orders: orderSlice.reducer,
+auth: authSlice.reducer,
 };
